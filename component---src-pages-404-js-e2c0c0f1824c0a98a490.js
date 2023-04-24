d566af0b91becd3a1b9aa341dd7ebb4378f7d753
y2meta <y2meta@proton.me>
2023-04-24 18:17:08 +0530

    Updates

---
+++ component---src-pages-404-js-e2c0c0f1824c0a98a490.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunky2meta=self.webpackChunky2meta||[]).push([[8883],{30429:function(e,t,l){l.r(t);var n=l(67294);t.default=()=>n.createElement("div",null,n.createElement("h1",null,"404: Not Found"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-e2c0c0f1824c0a98a490.js.map
