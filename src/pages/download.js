8704530e3bd26e901f077b92a28740cd27be905a
y2meta <y2meta@proton.me>
2023-05-25 19:21:32 +0530

    fix: added iframe style

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image"
+import React, {useState} from 'react';
+import {Link} from "gatsby";
+import {StaticImage} from "gatsby-plugin-image"
 import '../styles/global.css';
 
 const Download = (props) => {
   const [open, setOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState("en");
   const [openLanguage, setOpenLanguage] = useState(false);
+  const [openIframe, setOpenIframe] = useState(false);
   const url = props?.location?.state?.url;
 
   const handleLanguageChange = (language) => {
@@ -14,15 +15,22 @@ const Download = (props) => {
     setOpenLanguage(!openLanguage)
   };
 
-  if (typeof window !== 'undefined') {
-    (function (s, u, z, p) {
-      s.src = u;
-      s.setAttribute('data-zone', z);
-      p.appendChild(s);
-    })(document.createElement('script'), 'https://inklinkor.com/tag.min.js', 5870949, document.body || document.documentElement)
+  const handleClickDownload = () => {
+    window.open("//zaltaumi.net/4/5982524");
+    setOpenIframe(true)
   }
 
-  const languageOptions = [{ lang: "en", label: "English", path: "/download/" }];
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    const loadExternalScript = (domain, id) => {
+      const script = document.createElement("script");
+      script.src = `https://${domain}/401/${id}`;
+      (document.body || document.documentElement).appendChild(script);
+    };
+
+    loadExternalScript("glizauvo.net", 5982525);
+  }
+
+  const languageOptions = [{lang: "en", label: "English", path: "/download/"}];
   return (
     <>
       <div className="mx-auto md:max-w-[890px] px-3">
@@ -31,7 +39,8 @@ const Download = (props) => {
             <div className="flex justify-between items-center">
               <div className="lg:w-0 lg:flex-1">
                 <Link to="/" className="flex items-center">
-                  <StaticImage src="../images/logo.png" loading="eager" alt="y2meta" width={50} quality={50} />
+                  <StaticImage src="../images/logo.png" loading="eager" alt="y2meta" width={50}
+                               quality={50}/>
                   <h1 className="text-2xl font-semibold w-auto z-50 pl-2">y2meta</h1>
                 </Link>
               </div>
@@ -42,7 +51,8 @@ const Download = (props) => {
                   type="button"
                   className="inline-flex items-center justify-center border border-solid border-heading py-1 px-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                 >
-                  <StaticImage src="../images/mobile.svg" loading="eager" alt="y2meta" width={25} quality={25} />
+                  <StaticImage src="../images/mobile.svg" loading="eager" alt="y2meta" width={25}
+                               quality={25}/>
                 </button>
               </div>
               <nav className="hidden md:flex">
@@ -87,7 +97,7 @@ const Download = (props) => {
           </div>
           <div
             className="absolute top-16 inset-x-0 transition transform origin-top-right md:!hidden z-20"
-            style={{ display: open ? "block" : "none" }}>
+            style={{display: open ? "block" : "none"}}>
             <div className="shadow-lg">
               <div className="shadow-xs bg-background divide-y-2 divide-gray-50">
                 <div className="pt-5 pb-6 px-5 space-y-6">
@@ -115,7 +125,8 @@ const Download = (props) => {
                           {languageOptions.find((option) => option.lang === selectedLanguage)?.label || "Language"}
                         </button>
                         {openLanguage &&
-                          <div className="absolute mt-2 py-2 w-40 bg-white rounded-md shadow-lg">
+                          <div
+                            className="absolute mt-2 py-2 w-40 bg-white rounded-md shadow-lg">
                             {languageOptions.map((option) => (
                               <Link
                                 key={option.lang}
@@ -138,16 +149,58 @@ const Download = (props) => {
         </header>
         <section
           className="bg-white p-4 border border-solid rounded border-current	border-solid-clr container mx-auto">
-          <div className="md:py-8 py-7 text-center">
-            <div>
-              <iframe
-                className="h-[1000px]"
-                sandbox="allow-scripts allow-same-origin allow-presentation allow-downloads allow-forms"
-                src={`https://ytconvert.me/api/widgetv2?url=${url}`}
-                width="100%"
-              />
+          {!openIframe &&
+            <div className="p-8 my-20 max-w-xs mx-auto bg-white rounded-lg shadow-lg">
+              <div className="flex flex-col space-y-4">
+                <button onClick={handleClickDownload}
+                        className="bg-[#333c4d] hover:bg-[#030303] text-white font-bold py-3 px-4 rounded transition-colors duration-300">
+                  <svg
+                    className="animate-pulse inline-block w-5 h-5 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10L12 15 17 10M12 15V3"/>
+                  </svg>
+                  Download Now
+                </button>
+                <a
+                  className="bg-[#c3291a] text-center cursor-pointer hover:bg-[#c51200] text-white font-bold py-3 px-4 rounded transition-colors duration-300"
+                  href="//itespurrom.com/4/5982522"
+                  target="_blank" rel="noreferrer">
+                  <svg
+                    className="animate-pulse inline-block w-5 h-5 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M6 4l15 8-15 8V4z"/>
+                  </svg>
+                  Play Now
+                </a>
+              </div>
             </div>
-          </div>
+          }
+          {openIframe &&
+            <div className="md:py-8 py-7 text-center">
+              <div>
+                <iframe
+                  className="h-[1000px]"
+                  src={`https://ytconvert.me/api/widgetv2?url=${url}`}
+                  sandbox="allow-scripts allow-same-origin allow-presentation allow-downloads allow-forms"
+                  width="100%" height="100%" allowTransparency="true" scrolling="yes" style={{border: "none"}}
+                />
+              </div>
+            </div>
+          }
         </section>
       </div>
       <footer>
@@ -168,13 +221,13 @@ export default Download
 
 export const Head = () => (
   <>
-    <html lang="en" />
+    <html lang="en"/>
     <title>Y2meta - Free Youtube Downloader | Download Youtube Video</title>
     <meta
       name="description"
-      content="Y2meta is popular Free YouTube Downloader allow to Download YouTube video for Free with high quality in 1080p, 2160p, 2k, 4k, 8k without install software." />
-    <meta name="robots" content="noindex,nofollow" />
-    <link rel="icon" href="https://y2meta.mobi/icons/icon-256x256.png" />
-    <link rel="canonical" href="https://y2meta.mobi/download/" />
+      content="Y2meta is popular Free YouTube Downloader allow to Download YouTube video for Free with high quality in 1080p, 2160p, 2k, 4k, 8k without install software."/>
+    <meta name="robots" content="noindex,nofollow"/>
+    <link rel="icon" href="https://y2meta.mobi/icons/icon-256x256.png"/>
+    <link rel="canonical" href="https://y2meta.mobi/download/"/>
   </>
 )
